459e5216a644e459f09e791d17d754522bc72c56
eggplannt <a.oguntade11@gmail.com>
2025-05-22 23:00:49 -0600

    fixed edge cases and moved query to inline lua

--- lua/gotmpl/init.lua
+++ lua/gotmpl/init.lua
@@ -3,10 +3,21 @@ local M = {}
 M.setup = function()
 	vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
 		local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
-		local _, _, ext, _ = string.find(fname, ".*%.(%a+)(%.%a+)")
-		if ext then
-			metadata["injection.language"] = ext
+		local base_extention = fname:match("%.([a-z0-9_]+)%.tmpl$")
+		local ft = nil
+		if not base_extention then
+			base_extention = fname:match("^(.-)%.tmpl$")
+			---@diagnostic disable-next-line: cast-local-type
+			ft = vim.filetype.match({ filename = base_extention })
+		else
+			---@diagnostic disable-next-line: cast-local-type
+			ft = vim.filetype.match({ filename = "file." .. base_extention })
 		end
+		if not ft then
+			metadata["injection.language"] = "gotmpl"
+			return
+		end
+		metadata["injection.language"] = ft
 	end, {})
 	-- Make sure vim recognizes .tmpl files as gotmpl ft
 	vim.filetype.add({
@@ -14,6 +25,16 @@ M.setup = function()
 			tmpl = "gotmpl",
 		},
 	})
+
+	vim.treesitter.query.set(
+		"gotmpl",
+		"injections",
+		[[
+			((text) @injection.content
+				(#inject-go-tmpl!)
+				(#set! injection.combined))
+		]]
+	)
 end
 
 return M
